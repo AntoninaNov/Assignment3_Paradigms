313d8c3ae486f1ac5e129565138b421037030e43
AntNov <tonianovak@yahoo.com>
2023-10-21 01:40:16 +0300

    Implement initial structure for encrypt and decrypt functions

---
+++ decrypt.cpp
@@ -0,0 +1,11 @@
+#include "caesar.h"
+#include <cstring>
+
+char* decrypt(char* encryptedText, int key) {
+    int length = strlen(encryptedText);
+    char* decryptedText = new char[length + 1];
+
+    // TODO: Decryption logic here
+
+    return decryptedText;
+}
---
+++ encrypt.cpp
@@ -0,0 +1,11 @@
+#include "caesar.h"
+#include <cstring>
+
+char* encrypt(char* rawText, int key) {
+    int length = strlen(rawText);
+    char* encryptedText = new char[length + 1];
+
+    // TODO: Encryption logic here
+
+    return encryptedText;
+}
